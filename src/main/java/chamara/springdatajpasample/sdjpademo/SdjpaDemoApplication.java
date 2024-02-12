184df614fb150a532954ddcaa422564016e9b306
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-12 21:10:21 +0530

    initial commit

---
+++ src/main/java/chamara/springdatajpasample/sdjpademo/SdjpaDemoApplication.java
@@ -0,0 +1,13 @@
+package chamara.springdatajpasample.sdjpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdjpaDemoApplication.class, args);
+	}
+
+}
