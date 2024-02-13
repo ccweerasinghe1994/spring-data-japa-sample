0ea807a6639f37554ebbaf157b1ff62b9b447be5
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-14 05:20:39 +0530

    test setups

--- src/test/java/chamara/springdatajpasample/sdjpademo/SdjpaDemoApplicationTests.java
+++ src/test/java/chamara/springdatajpasample/sdjpademo/SdjpaDemoApplicationTests.java
@@ -1,10 +1,25 @@
 package chamara.springdatajpasample.sdjpademo;
 
+import chamara.springdatajpasample.sdjpademo.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class SdjpaDemoApplicationTests {
+
+	@Autowired
+	BookRepository bookRepository;
+
+	@Test
+	void itShouldName() {
+
+		long count = bookRepository.count();
+
+		assertThat(count).isGreaterThan(0);
+	}
 
 	@Test
 	void contextLoads() {
