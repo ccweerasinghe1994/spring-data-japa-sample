e8079953e48123f4ebc0b370e534640bcb7271ff
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-02-14 07:02:58 +0530

    test: it tests
    adding integration test

---
+++ src/test/java/chamara/springdatajpasample/sdjpademo/MySqlIT.java
@@ -0,0 +1,31 @@
+package chamara.springdatajpasample.sdjpademo;
+
+
+import chamara.springdatajpasample.sdjpademo.repositories.BookRepository;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ComponentScan(basePackages = {"chamara.springdatajpasample.sdjpademo.bootstrap"})
+@ActiveProfiles("local")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MySqlIT {
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    void testJPATestSliceTransaction() {
+        long count = bookRepository.count();
+        assertThat(count).isEqualTo(2);
+
+
+    }
+
+}
